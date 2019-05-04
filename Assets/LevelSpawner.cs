6e9a3c2936cbdef145f127a82f7ed20e383e9de0
drwuro <zeha@drwuro.com>
2019-05-05 00:58:46 +0200

    moved level data into its own file

--- Assets/LevelSpawner.cs
+++ Assets/LevelSpawner.cs
@@ -11,53 +11,28 @@ public class LevelSpawner : MonoBehaviour
     private int levelwidth = 8;
     private int levelheight = 8;
     private int numfloors = 4;
-    
-    private char[,,] level = { { {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'} },
-
-                                 { {'#', '#', ' ', '#', '#', '#', '#', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', ' ', '#', '#', '#', '#', ' ', '#'},
-                                   {'#', ' ', '#', '#', ' ', '#', ' ', '#'},
-                                   {'#', ' ', '#', ' ', ' ', '#', ' ', '#'},
-                                   {'#', ' ', '#', '#', ' ', '#', ' ', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'} },
-
-                                 { {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', ' ', ' ', ' ', ' ', '#', '#'},
-                                   {'#', '#', ' ', ' ', ' ', ' ', '#', '#'},
-                                   {'#', '#', ' ', ' ', ' ', ' ', '#', '#'},
-                                   {'#', '#', ' ', ' ', ' ', ' ', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'} },
-
-                                 { {'#', '#', '#', '#', '#', '#', '#', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-                                   {'#', '#', '#', '#', '#', '#', '#', '#'} } };
 
     // Start is called before the first frame update
     void Start()
     {
+        TextAsset txt = (TextAsset)Resources.Load("level1", typeof(TextAsset));  
+        string[] lines = txt.text.Split('\n');
+
+        int.TryParse(lines[0], out levelwidth);
+        int.TryParse(lines[1], out levelheight);
+        int.TryParse(lines[2], out numfloors);
+        
+        Debug.Log("" + levelwidth + " " + levelheight + " " + numfloors);
+        
         for(int y=0; y<numfloors; y++)
         {
             for(int z=0; z<levelheight; z++)
             {
                 for(int x=0; x<levelwidth; x++)
                 {
-                    char tile = level[y,x,z];
+                    string tileline = lines[y * (levelheight + 1) + 4 + z];
+                    char tile = tileline[x];
+                    
                     Vector3 pos = new Vector3(offset.x + x * tilesize, offset.y + y * tilesize, offset.z + z * tilesize);
                     
                     switch(tile)
