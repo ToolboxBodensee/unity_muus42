15c69809011b13d46398938524b8e866939228d2
drwuro <zeha@drwuro.com>
2019-05-05 03:05:20 +0200

    added new mouse model with all animations and also added bad sunset skybox

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,10 +10,13 @@ public class PlayerMovement : MonoBehaviour
 
     private bool canJump = true;
     
+    private Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject theRealMouse = GameObject.Find("maus+animations");
+        animator = theRealMouse.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,19 +31,26 @@ public class PlayerMovement : MonoBehaviour
         
         rb.AddForce(transform.forward * walkSpeed * axisVertical);
         
+        if (Mathf.Abs(axisVertical) > 0.05)
+            animator.SetBool("walking", true);
+        else
+            animator.SetBool("walking", false);
+        
         if (Input.GetButton("Fire1") && canJump)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             canJump = false;
+            animator.SetBool("jumping", true);
         }
         
         RaycastHit rh;
         Physics.Raycast(transform.position, Vector3.down, out rh);
         
         if (rh.distance <= 0.55)
+        {
             canJump = true;
-            
-        Debug.Log("can jump: " + canJump);
+            animator.SetBool("jumping", false);
+        }
             
         Debug.DrawRay(transform.position, Vector3.down * rh.distance, Color.yellow);
     }
